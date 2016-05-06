1f540bba81fd58a4d5cceb3d90303ee36dab54dd
Kousik Sundar <kousiksundar@gmail.com>
2016-05-05 22:15:32 -0700

    Finished implimenting retries. input.json should only have 1 object. Arrays are not suppoerted yet. Will create a discussion on canvas for this.

--- assignment3/app.go
+++ assignment3/app.go
@@ -5,11 +5,10 @@ import (
     "os"
     "strconv"
     "strings"
-    // /"time"
     "encoding/json"
     "io/ioutil"
+    "time"
     "net/http"
-    //"gopkg.in/mgo.v2/bson"
     "bytes"
     "github.com/jasonlvhit/gocron"
 )
@@ -43,7 +42,6 @@ type OutputBSON struct{
     Callback_response_code int `json:"callback_response_code"`
 }
 
-
 type HttpResponse struct {
 	Response struct{
 		Http_response_code int `json:"http_response_code"`
@@ -61,11 +59,12 @@ type HttpResponse struct {
 var input InputBSON
 var httpResponse HttpResponse
 var outputBSON OutputBSON
+var requestsYetToSucceed int
 
-func doRequest(){
+func doInitialRequest(){
 	//Start Make initial Request
 		url :=input.Request.Url
-	    fmt.Println("Making request to: ", url)
+	    fmt.Println("\nMaking initial request for: ", url)
 	    jsonStr,_:=json.Marshal(input.Request.Body)
 	    jsonBody := []byte(jsonStr)
 	    method:=input.Request.Method
@@ -98,12 +97,12 @@ func doRequest(){
 	    httpResponse.Response.Body.Hello=  string(body)
 	//End filling HttpResponse
 	
-	//Start fill Input and Output in OutputBSOn
+	//Start fill Input, Output, Num_retries in OutputBSON
 		outputBSON.Job.Num_retries=0
 		outputBSON.Input=input
 		outputBSON.Output=httpResponse
 		outputBSON.Job.Num_retries=outputBSON.Job.Num_retries+1
-	//End fill Input and Output in OutputBSOn
+	//End fill Input, Output, Num_retries in OutputBSON
 
 	//If success do callback webhook
 	    if resp.Status == "200 OK"{
@@ -115,12 +114,15 @@ func doRequest(){
 	    	s:=strings.Split(resp.Status," ")
 			Status,_:=strconv.Atoi(s[0])
 	    	outputBSON.Callback_response_code=Status
+	    	fmt.Println("Request was Completed Successfully. Status is COMPLETED")
 	    }
-	//End Callback
+	//End Callback webhook
 
 	//If failure, put Still Trying and write to output. Leave callback response code empty
 	    if resp.Status != "200 OK"{
 	    	outputBSON.Job.Status="STILL_TRYING"
+	    	requestsYetToSucceed=requestsYetToSucceed+1
+	    	fmt.Println("Request was Unsuccessful. Status is STILL_TRYING. You have "+strconv.Itoa(input.Max_retries)+" retries left.")
 	    }
 	//End if failure
 
@@ -129,43 +131,143 @@ func doRequest(){
 	    writeFile([]byte(a),"output.bson")
 }
 
+func doRetry(){
+	f, _ := os.Open("output.bson")
+	defer f.Close()
+	buf, _ := ioutil.ReadAll(f)
+    json.Unmarshal(buf, &outputBSON);
 
+    if outputBSON.Job.Status=="STILL_TRYING" {
+    	//Check if num of retries is expired
+    	if outputBSON.Job.Num_retries==outputBSON.Input.Max_retries {
+    		outputBSON.Job.Status="FAILED"
+    		//Do Callback
+    			req,_ := http.NewRequest("POST", outputBSON.Input.Callback_webhook_url, bytes.NewBuffer([]byte("")))
+		    	client := &http.Client{}
+		    	resp,_ := client.Do(req)
+		    	s:=strings.Split(resp.Status," ")
+				Status,_:=strconv.Atoi(s[0])
+		    	outputBSON.Callback_response_code=Status
+    		//End Callback
 
+		    //Write to output.bson
+		    	outputBSON.Input.Id=makeTimestamp()
+		    	a,_:=json.Marshal(outputBSON)
+	    		writeFile([]byte(a),"output.bson")
+	    	//End write to output
 
+	    	//Update counter
+	    	requestsYetToSucceed=requestsYetToSucceed-1
+
+	    	fmt.Println("\nSorry. Retries Maxed Out. FAILED.")
+    	}
+
+    	//Check if num of retries < max retries
+    	if outputBSON.Job.Num_retries < outputBSON.Input.Max_retries {
+    		//Start Make Retry Request
+				url :=outputBSON.Input.Request.Url
+			    fmt.Println("\nMaking Retry request to: ", url)
+			    jsonStr,_:=json.Marshal(input.Request.Body)
+			    jsonBody := []byte(jsonStr)
+			    method:=input.Request.Method
+			    req, _ := http.NewRequest(method, url, bytes.NewBuffer(jsonBody))
+			    content_type:=input.Request.Http_headers.Content_Type
+			    accept:=input.Request.Http_headers.Accept
+			    req.Header.Set("Accept", accept)
+			    req.Header.Set("Content-Type", content_type)
+			    client := &http.Client{}
+			    resp, _ := client.Do(req)
+			    defer resp.Body.Close()
+    		//End Make Retry Request
+
+			//Start filling HttpResponse
+			   		//Status
+			    s:=strings.Split(resp.Status," ")
+				Status,_:=strconv.Atoi(s[0])
+				httpResponse.Response.Http_response_code=Status
+					//Date
+			    Date:=resp.Header["Date"][0]
+			    httpResponse.Response.Http_headers.Date=Date
+			    	//Content-Type
+			    Content_type:=resp.Header["Content-Type"][0]
+			    httpResponse.Response.Http_headers.Content_Type=Content_type
+			    	//Content-Length
+			    Content_length:=resp.Header["Content-Length"][0]
+			    httpResponse.Response.Http_headers.Content_Length,_=strconv.Atoi(Content_length)
+			    	//Body
+			    body, _ := ioutil.ReadAll(resp.Body)
+			    httpResponse.Response.Body.Hello=  string(body)
+			//End filling HttpResponse
+
+		    //Start fill Input, Output, Num_retries in OutputBSON
+				outputBSON.Output=httpResponse
+				outputBSON.Job.Num_retries=outputBSON.Job.Num_retries+1
+			//End fill Input, Output, Num_retries in OutputBSON
+
+			//If success do callback webhook
+			    if resp.Status == "200 OK"{
+			    	outputBSON.Job.Status="COMPLETED"
+			    	//Do callback
+			    	req,_ := http.NewRequest("POST", outputBSON.Input.Callback_webhook_url, bytes.NewBuffer([]byte("")))
+			    	client := &http.Client{}
+			    	resp,_ := client.Do(req)
+			    	s:=strings.Split(resp.Status," ")
+					Status,_:=strconv.Atoi(s[0])
+			    	outputBSON.Callback_response_code=Status
+			    	requestsYetToSucceed=requestsYetToSucceed-1
+			    	fmt.Println("Request was completed Successfully. Status is COMPLETED.")
+			    }
+			//End Callback webhook
+
+			//If failure, put Still Trying and write to output. Leave callback response code empty
+			    if resp.Status != "200 OK"{
+			    	outputBSON.Job.Status="STILL_TRYING"
+			    	fmt.Println("Request was Unsuccessful. Status is STILL_TRYING. You have "+strconv.Itoa(outputBSON.Input.Max_retries-outputBSON.Job.Num_retries+1)+" retries left.")
+			    }
+			//End if failure
+
+			//Write to output
+			outputBSON.Input.Id=makeTimestamp()
+	    	a,_:=json.Marshal(outputBSON)
+	    	writeFile([]byte(a),"output.bson")
+    	}
+    } 	
+}
+
+//Helper Function to write to output.json
 func writeFile(data []byte, fileName string)  {
     file, _ := os.Create(fileName)
     defer file.Close()   
-    numBytes, _ := file.Write(data)
-    fmt.Printf("\nwrote %d bytes to %s\n", numBytes, fileName)
+    file.Write(data)
     file.Sync()
 }
+
+//Helper function for Timestamp
+func makeTimestamp() int64 {
+    return time.Now().UnixNano() / (int64(time.Millisecond)/int64(time.Nanosecond))
+}
+
 func task() {
+	//Initialize
+	requestsYetToSucceed=0
 	//Get input.bson
 	arg := os.Args[1]
 	f, _ := os.Open(arg)
 	defer f.Close()
 	buf, _ := ioutil.ReadAll(f)
     json.Unmarshal(buf, &input); 
-
     //Do the required Http request
-    doRequest()
-
-
-
-
-
-    // t := makeTimestamp()
-    // fmt.Printf("Running task...@%d\n", t)
-    // data := createBSON()
-    // writeFile(data, "data.bson")
-    // readFile("data.bson")
+    doInitialRequest()
+    //Do Retries
+    for requestsYetToSucceed > 0 {
+    	doRetry()
+    }
+    fmt.Println("\nAll requests are now COMPLETED or FAILED\n")
 }
 
 func main() {
-
-	
-
     s := gocron.NewScheduler()
     s.Every(5).Seconds().Do(task)
     <- s.Start()
+    task()
 }
